acda6a5ea2ae4fa4d08eaebf80e2a867e05dc11c
Nandamuri-Pavithra <pavithranandamuri20@gmail.com>
2023-07-28 14:10:11 +0000

    Queries

--- pavithra.sql
+++ pavithra.sql
@@ -1,6 +1,6 @@
 set timing on;
 --Write a SQL Query to find the employee count for each agency
-select "AGENCY",count(*) num_employees from HR_DEPARTMENT
+select "AGENCY",count(*) as num_employees from HR_DEPARTMENT
 group by "AGENCY"; 
 
 --Write a SQL Query to list the employees who are all CONTRACTUAL WORKERS?
@@ -12,11 +12,11 @@ select "Employee Name" from hr_department WHERE
  "AGENCY" = 'ABE LINCOLN PRES LIBRARY MUS2';
 
 --Write a SQL Query to find the employees count in each position?
-select "Position Title", count(*)  num_employees
+select "Position Title", count(*) as num_employees
  from HR_DEPARTMENT
  group by "Position Title";  
  
 --Write a SQL Query to count the number of employees whose period pay rate is less than 50000?
- select count(*) num_employees from HR_DEPARTMENT
+ select count(*) as num_employees from HR_DEPARTMENT
  where "Period Pay Rate" < 50000;   
 
