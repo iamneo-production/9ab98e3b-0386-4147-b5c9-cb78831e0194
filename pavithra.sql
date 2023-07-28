e47d9b80ef56e950561ae9f13f0f6a9c4011bec3
Nandamuri-Pavithra <pavithranandamuri20@gmail.com>
2023-07-28 13:46:19 +0000

    final codes

--- pavithra.sql
+++ pavithra.sql
@@ -1,17 +1,22 @@
 set timing on;
+--Write a SQL Query to find the employee count for each agency
 select "AGENCY",count(*) num_employees from HR_DEPARTMENT
-group by "AGENCY";   ----00.03
+group by "AGENCY"; 
 
+--Write a SQL Query to list the employees who are all CONTRACTUAL WORKERS?
 select "Employee Name" from HR_DEPARTMENT
-where "Position Title" = 'CONTRACTUAL WORKER'; ----00.06
+where "Position Title" = 'CONTRACTUAL WORKER';
 
+--Write a SQL Query to list the employee name work under Agency ABE LINCOLN PRES LIBRARY MUS2?
 select "Employee Name" from hr_department WHERE
- "AGENCY" = 'ABE LINCOLN PRES LIBRARY MUS2';  ---00.03
+ "AGENCY" = 'ABE LINCOLN PRES LIBRARY MUS2';
 
+--Write a SQL Query to find the employees count in each position?
 select "Position Title", count(*)  num_employees
  from HR_DEPARTMENT
- group by "Position Title";   --- elapsed 00.06
-
+ group by "Position Title";  
+ 
+--Write a SQL Query to count the number of employees whose period pay rate is less than 50000?
  select count(*) num_employees from HR_DEPARTMENT
- where "Period Pay Rate" < 5000;   ----00.03
+ where "Period Pay Rate" < 50000;   
 
