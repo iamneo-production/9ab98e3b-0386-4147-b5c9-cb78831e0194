3a2642cc1a1b32a0b8071b6cda1cc09a29d39f53
pradeepavanapalli <pradeepaysp@gmail.com>
2023-07-29 17:23:56 +0000

    pradeepa

--- code.sql
+++
@@ -1,6 +0,0 @@
-set timing on;
-select agency, count(*) employees_count  from HR_DEPARTMENT
-group by agency; --Elapsed:  00:00:00:07
-
-select * EMPLOYEE_NAME from HR_DEPARTMENT
-where POSITION_TITL = 'CONTRACTUAL WORKER';
