ffcb28d5f9a5454b3f7b780a5049035ea8f22f16
Pavan-kumar0 <pavankumarvasa99@gmail.com>
2023-07-29 08:57:48 +0000

    commit

--- pavanvasa.sql
+++ pavanvasa.sql
@@ -1,23 +1,22 @@
 set timing on;
 
---write a sql query to find the employees count from each agency?
+-- write a query to  find the employee count for each agency 
+select agency,count(*) as employee_count from hr_department 
+group by agency;
 
-select agency,count(*)as employee_count from HR_DEPARTMENT 
-group by agency; 
+-- write a query to list the employees who are all contractual worker 
 
---Write an SQL query to list the employees who are all contractual workers?
-SELECT "Employee Name" as "CONTRACTUAL WORKERS" FROM HR_DEPARTMENT
-WHERE "Position Title" = 'CONTRACTUAL WORKER'; 
+select "Employee Name" as "CONTRACTUAL WORKERS" from hr_department where "Position Title" ='CONTRACTUAL WORKER';
 
---write an SQL query to list the employee name work under agency ABE LINCOLN PRESLIBARARY&MUS?
-select "Employee Name" from HR_DEPARTMENT 
-where "AGENCY"='ABE LINCOLN PRES LIBRARY & MUS';
+-- write query to list the employee name work under agency ABE LINCOLN PRES LIBRARY MUS2 
 
---Write an SQL query to find the employee count in each position?
-select "Position Title",count(*) as employee_count from HR_DEPARTMENT
-group by "Position Title"; 
+select "Employee Name" from hr_department where agency='ABE LINCOLN PRES LIBRARY MUS2';
 
---Write an SQL query to count the no.of employees whose period pay rate less then 50000? 
-select count(*) as employee_count from HR_DEPARTMENT
-where "Period Pay Rate"<50000;
 
+-- write an sql query to find the employees count in each position 
+select "Position Title",count(*) as employee_count from hr_department group by "Position Title";
+
+
+--write an sql query to count the number of employees whose period pay rate is less then 50000 
+
+select count(*) as employee_count from hr_department where "Period Pay Rate" <50000;
