55b2dbd5d88e859823aa1f9ed17f83a2390c5285
SaiPriya568 <priyabujjik@gmail.com>
2023-07-30 12:34:41 +0000

    Commit1

--- priya.sql
+++ priya.sql
@@ -1,12 +1,22 @@
 set timing on;
-select * from hr_department;
 
-select agency, COUNT(*) as EmployeeCount from HR_DEPARTMENT group by agency; --Elapsed:  00:00:00:03
+-- write a query to  find the employee count for each agency 
+select agency,count(*) as employee_count from hr_department 
+group by agency;
 
-select * from hr_department where "Position Title" = 'CONTRACTUAL WORKER';  --Elapsed:  00:00:00:06
+-- write a query to list the employees who are all contractual worker 
 
-select "Employee Name" from HR_DEPARTMENT where agency = 'ade lincoln pres library mus2'; --Elapsed:  00:00:00:02
+select "Employee Name" as "CONTRACTUAL WORKERS" from hr_department where "Position Title" ='CONTRACTUAL WORKER';
 
-select "Position Title", count(*) as EmployeeCount from hr_department group by "Position Title"; --Elapsed:  00:00:00:06
+-- write query to list the employee name work under agency ABE LINCOLN PRES LIBRARY MUS2 
 
-select count(*) as EmployeeCount from hr_department where "Period Pay Rate" < 50000; --Elapsed:  00:00:00:02
+select "Employee Name" from hr_department where agency='ABE LINCOLN PRES LIBRARY MUS2';
+
+
+-- write an sql query to find the employees count in each position 
+select "Position Title",count(*) as employee_count from hr_department group by "Position Title";
+
+
+--write an sql query to count the number of employees whose period pay rate is less then 50000 
+
+select count(*) as employee_count from hr_department where "Period Pay Rate" <50000;
