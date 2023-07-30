495d7b13c6c73c75e12cbcc2223dab81716e1761
Nandamuri-Pavithra <pavithranandamuri20@gmail.com>
2023-07-30 16:55:34 +0000

    final commit

--- pavithra.sql
+++ pavithra.sql
@@ -3,6 +3,8 @@ set timing on;
 select "AGENCY",count(*) as num_employees from HR_DEPARTMENT
 group by "AGENCY"; 
 
+
+
 --Write a SQL Query to list the employees who are all CONTRACTUAL WORKERS?
 select "Employee Name" from HR_DEPARTMENT
 where "Position Title" = 'CONTRACTUAL WORKER';
@@ -10,6 +12,7 @@ where "Position Title" = 'CONTRACTUAL WORKER';
 --Write a SQL Query to list the employee name work under Agency ABE LINCOLN PRES LIBRARY MUS2?
 select "Employee Name" from hr_department WHERE
  "AGENCY" = 'ABE LINCOLN PRES LIBRARY MUS2';
+
 
 --Write a SQL Query to find the employees count in each position?
 select "Position Title", count(*) as num_employees
