2ab37eec7009a88142df6538a2650e5d5a313931
Pavan-kumar0 <pavankumarvasa99@gmail.com>
2023-07-30 14:12:38 +0000

    COMMITED

--- pavan_vasa.sql
+++ pavan_vasa.sql
@@ -12,10 +12,8 @@ select "Employee Name" as "CONTRACTUAL WORKERS" from hr_department where "Positi
 
 select "Employee Name" from hr_department where agency='ABE LINCOLN PRES LIBRARY MUS2';
 
-
 -- write an sql query to find the employees count in each position 
 select "Position Title",count(*) as employee_count from hr_department group by "Position Title";
-
 
 --write an sql query to count the number of employees whose period pay rate is less then 50000 
 
